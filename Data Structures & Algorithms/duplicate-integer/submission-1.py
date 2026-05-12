740ddb918bed916ec3cd2478b1e066a830e995ee
Lily <116549104+LilyDao510@users.noreply.github.com>
2026-05-12 11:04:34 -0700

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,49 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # Cach 1
+
+        # map= {}
+        # for i in nums:
+        #     if i in map:
+        #         map[i] += 1
+        #         if map[i] >1:
+        #             return True
+        #     else:
+        #         map[i] = 1
+        # return False
+
+        # Cach 2
+
+        # my_set = set()
+        # for i in nums:
+        #     if i in my_set:
+        #         return True
+        #     else:
+        #         my_set.add(i)
+        # return False
+        
+        
+        
+        
+        new_map = {}
+        l = len(nums)
+        for i in range(l):
+            if nums[i] in new_map:
+                new_map[nums[i]] += 1
+            else:
+                new_map[nums[i]] = 1
+
+            if new_map[nums[i]]> 1:
+                return True
+        return False
+        
+
+
+
+
+        
+       
+
+
+            
+        
